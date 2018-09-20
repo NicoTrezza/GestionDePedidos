b210a3c14122747dc6b1faba803dd517bd47a722
olmosmartin <tiincho18@gmail.com>
2018-09-20 11:44:19 -0300

    primera version del server
    por ahora solo levanta el index

---
+++ prototipo/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder="vistas")
+
+@app.route('/')# rutas a las que el usuario puede entrar
+def index():
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug = True, port = 8000) #ejecuta el server
