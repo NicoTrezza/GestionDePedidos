ffcda2c5ca09ee406fdcae7bb4f83a57f3b5e52f
olmosmartin <tiincho18@gmail.com>
2018-09-21 23:32:23 -0300

    creado template base
    ahora las vistan van a heredar de una vista que va a hacer de base para las demás llamada "base.html"

--- prototipo/main.py
+++ prototipo/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder="vistas")
 
 @app.route('/')# rutas a las que el usuario puede entrar
 def index():
-    return render_template('index.html')
+    return render_template('index.html', titulo="Campus Gestion")
 
 
 if __name__=='__main__':
