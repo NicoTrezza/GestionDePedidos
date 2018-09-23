32ae701ca68581652341fb2d6b55017c440e3671
olmosmartin <tiincho18@gmail.com>
2018-09-22 21:31:20 -0300

    habilitadas rutas
    estan puestas las rutas para poder acceder a todas las vistas

--- prototipo/main.py
+++ prototipo/main.py
@@ -7,6 +7,30 @@ app = Flask(__name__, template_folder="vistas")
 @app.route('/')# rutas a las que el usuario puede entrar
 def index():
     return render_template('index.html', titulo="Campus Gestion")
+	
+@app.route('/usuario/matricular')
+def matricular():
+    return render_template('Usuario/matricular.html', titulo="Matricular")
+	
+@app.route('/aula/crear')
+def crear():
+    return render_template('Aula/crear.html', titulo="Crear aula")
+	
+@app.route('/aula/reutilizar')
+def reutilizar():
+    return render_template('Aula/reutilizar.html', titulo="Reutilizar aula")
+	
+@app.route('/aula/eliminar')
+def eliminar():
+    return render_template('Aula/eliminar.html', titulo="Eliminar aula")
+	
+@app.route('/capacitacion/tutorias')
+def tutorias():
+    return render_template('Capacitacion/tutorias.html', titulo="Tutorias")
+	
+@app.route('/capacitacion/microtalleres')
+def microtalleres():
+    return render_template('Capacitacion/microtalleres.html', titulo="Microtalleres")
 
 
 if __name__=='__main__':
