72f90451e611d61e8a5c4cfc2b6215e9560e32c5
olmosmartin <tiincho18@gmail.com>
2018-10-11 20:55:53 -0300

    boton de descarga de archivo arreglado
    ahora se peude bajar el xlsx de matriculacion

--- prototipo/main.py
+++ prototipo/main.py
@@ -2,19 +2,27 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_file
 from negocio.aulaABM import AulaABM
 
 app = Flask(__name__, template_folder="vistas")
 
-@app.route('/')# rutas a las que el usuario puede entrar
+
+@app.route('/')  # rutas a las que el usuario puede entrar
 def index():
     return render_template('index.html', titulo="Campus Gestion")
-	
+
+
 @app.route('/usuario/matricular')
 def matricular():
     return render_template('Usuario/matricular.html', titulo="Matricular")
-	
-@app.route('/aula/crear', methods = ['GET', 'POST'])
+
+@app.route('/return_file/')
+def devolverarchivo():
+    return send_file('C:/Users/martin/Desktop/proyecto software/GestorDePedidos/prototipo/vistas/Archivos/Formulario_de_matriculacion.xlsx', attachment_filename='Formulario_de_matriculacion.xlsx')
+
+
+@app.route('/aula/crear', methods=['GET', 'POST'])
 def crear():
     if request.method == 'POST':
         abm = AulaABM()
@@ -25,25 +33,30 @@ def crear():
         print request.form['nombre']
         print request.form['apellido']
         print request.form['dni']
-        abm.insertar(request.form['dependencia'],request.form['nombreaula'],request.form['nombreprofesor'],request.form['email'])
+        abm.insertar(request.form['dependencia'], request.form['nombreaula'], request.form['nombreprofesor'],
+                     request.form['email'])
     return render_template('Aula/crear.html', titulo="Crear aula")
-	
+
+
 @app.route('/aula/reutilizar')
 def reutilizar():
     return render_template('Aula/reutilizar.html', titulo="Reutilizar aula")
-	
+
+
 @app.route('/aula/eliminar')
 def eliminar():
     return render_template('Aula/eliminar.html', titulo="Eliminar aula")
-	
+
+
 @app.route('/capacitacion/tutorias')
 def tutorias():
     return render_template('Capacitacion/tutorias.html', titulo="Tutorias")
-	
+
+
 @app.route('/capacitacion/microtalleres')
 def microtalleres():
     return render_template('Capacitacion/microtalleres.html', titulo="Microtalleres")
 
 
-if __name__=='__main__':
-    app.run(debug = True, port = 8000) #ejecuta el server
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)  # ejecuta el server
