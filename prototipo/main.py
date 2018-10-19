a7049aefe41b8e3be62815b5d3361c6508be6000
olmosmartin <tiincho18@gmail.com>
2018-10-19 19:22:39 -0300

    mensajes de permisos
    mensajes si el usuario noe stá logueado o no tienen los permisos necesarios para enviar un form

--- prototipo/main.py
+++ prototipo/main.py
@@ -4,10 +4,12 @@ from flask import render_template
 from flask import request
 from flask import send_file
 from flask_wtf import CSRFProtect
-
 from flask import session
 from flask import url_for
 from flask import redirect
+
+from flask import flash
+
 from negocio.aulaABM import AulaABM
 
 import forms
@@ -31,9 +33,12 @@ def index():
 def login():
     login = forms.Login(request.form)
     if request.method == 'POST' and login.validate():
+        #print login.usuario.data
+        #print login.contrasenia.data
+        mensajeBienvenida = 'Bienvenido {}'.format(login.usuario.data)
+        flash(mensajeBienvenida)
+
         session['usuario'] = login.usuario.data
-        print login.usuario.data
-        print login.contrasenia.data
     return render_template('Login/login.html', titulo="Login", form=login)
 
 
@@ -47,9 +52,17 @@ def logout():
 @app.route('/usuario/matricular', methods=['GET', 'POST'])
 def matricular():
     matricular = forms.Matricular(request.form)
-    if request.method == 'POST' and matricular.validate() and session['usuario'] == usuariovalido:
-        print matricular.departamento.data
-        print matricular.carrera.data
+    usuario = ''
+    if request.method == 'POST' and matricular.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                print matricular.departamento.data
+                print matricular.carrera.data
+                usuario = session['usuario']
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para matricular')
     return render_template('Usuario/matricular.html', titulo="Matricular", form=matricular)
 
 
@@ -63,83 +76,122 @@ def devolverarchivo():
 @app.route('/aula/crear', methods=['GET', 'POST'])
 def crear():
     crear_aula = forms.CrearAula(request.form)
-    if request.method == 'POST' and crear_aula.validate() and session['usuario'] == usuariovalido:
-        # abm = AulaABM()
-        print crear_aula.departamento.data
-        print crear_aula.carrera.data
-        print crear_aula.nombreaula.data
-        print crear_aula.nombredirector.data
-        print crear_aula.emaildirector.data
-        print crear_aula.nombredocente.data
-        print crear_aula.apellidodocente.data
-        print crear_aula.dni.data
-        print crear_aula.emailprofesor.data
-        print crear_aula.rol.data
-        print crear_aula.descripcion.data
-
-        # abm.insertar(crear_aula.dependencia.data, crear_aula.nombreAula.data, crear_aula.nombreprofesor.data,
-        #             crear_aula.email.data)
+    usuario = ''
+    if request.method == 'POST' and crear_aula.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                # abm = AulaABM()
+                print crear_aula.departamento.data
+                print crear_aula.carrera.data
+                print crear_aula.nombreaula.data
+                print crear_aula.nombredirector.data
+                print crear_aula.emaildirector.data
+                print crear_aula.nombredocente.data
+                print crear_aula.apellidodocente.data
+                print crear_aula.dni.data
+                print crear_aula.emailprofesor.data
+                print crear_aula.rol.data
+                print crear_aula.descripcion.data
+                usuario = session['usuario']
+                # abm.insertar(crear_aula.dependencia.data, crear_aula.nombreAula.data, crear_aula.nombreprofesor.data,
+                #             crear_aula.email.data)
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para crear aula')
     return render_template('Aula/crear.html', titulo="Crear aula", form=crear_aula)
 
 
 @app.route('/aula/reutilizar', methods=['GET', 'POST'])
 def reutilizar():
     reutilizar_aula = forms.ReutilizarAula(request.form)
-    if request.method == 'POST' and reutilizar_aula.validate() and session['usuario'] == usuariovalido:
-        print reutilizar_aula.departamento.data
-        print reutilizar_aula.carrera.data
-        print reutilizar_aula.nombreaula.data
-        print reutilizar_aula.direccionulr.data
-        print reutilizar_aula.nombredirector.data
-        print reutilizar_aula.emaildirector.data
-        print reutilizar_aula.nombrenuevo.data
-        print reutilizar_aula.otro.data
+    usuario = ''
+    if request.method == 'POST' and reutilizar_aula.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                print reutilizar_aula.departamento.data
+                print reutilizar_aula.carrera.data
+                print reutilizar_aula.nombreaula.data
+                print reutilizar_aula.direccionulr.data
+                print reutilizar_aula.nombredirector.data
+                print reutilizar_aula.emaildirector.data
+                print reutilizar_aula.nombrenuevo.data
+                print reutilizar_aula.otro.data
+                usuario = session['usuario']
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para reutilizar aula')
     return render_template('Aula/reutilizar.html', titulo="Reutilizar aula", form=reutilizar_aula)
 
 
 @app.route('/aula/eliminar', methods=['GET', 'POST'])
 def eliminar():
     eliminar_aula = forms.EliminarAula(request.form)
-    if request.method == 'POST' and eliminar_aula.validate() and session['usuario'] == usuariovalido:
-        print eliminar_aula.departamento.data
-        print eliminar_aula.carrera.data
-        print eliminar_aula.nombreaula.data
-        print eliminar_aula.direccionulr.data
-        print eliminar_aula.nombredirector.data
-        print eliminar_aula.emaildirector.data
-        print eliminar_aula.motivo.data
+    usuario = ''
+    if request.method == 'POST' and eliminar_aula.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                print eliminar_aula.departamento.data
+                print eliminar_aula.carrera.data
+                print eliminar_aula.nombreaula.data
+                print eliminar_aula.direccionulr.data
+                print eliminar_aula.nombredirector.data
+                print eliminar_aula.emaildirector.data
+                print eliminar_aula.motivo.data
+                usuario = session['usuario']
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para eliminar aula')
     return render_template('Aula/eliminar.html', titulo="Eliminar aula", form=eliminar_aula)
 
 
 @app.route('/capacitacion/tutorias', methods=['GET', 'POST'])
 def tutorias():
     tutorias = forms.Tutorias(request.form)
-    if request.method == 'POST' and tutorias.validate() and session['usuario'] == usuariovalido:
-        print tutorias.motivo.data
-        print tutorias.nombre.data
-        print tutorias.apellido.data
-        print tutorias.email.data
-        print tutorias.telefono.data
-        print tutorias.dni.data
-        print tutorias.fecha.data
-        print tutorias.departamento.data
-        print tutorias.carrera.data
-        print tutorias.rol.data
+    usuario = ''
+    if request.method == 'POST' and tutorias.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                print tutorias.motivo.data
+                print tutorias.nombre.data
+                print tutorias.apellido.data
+                print tutorias.email.data
+                print tutorias.telefono.data
+                print tutorias.dni.data
+                print tutorias.fecha.data
+                print tutorias.departamento.data
+                print tutorias.carrera.data
+                print tutorias.rol.data
+                usuario = session['usuario']
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para pedir tutoria')
     return render_template('Capacitacion/tutorias.html', titulo="Tutorias", form=tutorias)
 
 
 @app.route('/capacitacion/microtalleres', methods=['GET', 'POST'])
 def microtalleres():
     microtalleres = forms.Microtalleres(request.form)
-    if request.method == 'POST' and microtalleres.validate() and session['usuario'] == usuariovalido:
-        print microtalleres.nombre.data
-        print microtalleres.apellido.data
-        print microtalleres.email.data
-        print microtalleres.telefono.data
-        print microtalleres.dni.data
-        print microtalleres.departamento.data
-        print microtalleres.carrera.data
-        print microtalleres.motivo.data
+    usuario = ''
+    if request.method == 'POST' and microtalleres.validate():
+        if 'usuario' in session:
+            if session['usuario'] == usuariovalido:
+                print microtalleres.nombre.data
+                print microtalleres.apellido.data
+                print microtalleres.email.data
+                print microtalleres.telefono.data
+                print microtalleres.dni.data
+                print microtalleres.departamento.data
+                print microtalleres.carrera.data
+                print microtalleres.motivo.data
+                usuario = session['usuario']
+            else:
+                flash('No tiene permisos')
+        if usuario == '':
+            flash('Necesita estar logueado para pedir microtaller')
     return render_template('Capacitacion/microtalleres.html', titulo="Microtalleres", form=microtalleres)
 
 
