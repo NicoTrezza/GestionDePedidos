505658cae354c75da44ef17085c0c223762799f2
jemi14 <griselda_gsis@hotmail.com>
2018-10-31 18:22:04 -0300

    Datos
    Tanto rojos.... me trae recuerdos de ayed(?

--- prototipo/test.py
+++ prototipo/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from negocio.departamentoABM import DepartamentoABM
 from negocio.carreraABM import CarreraABM
 
@@ -5,9 +7,27 @@ def main():
 	departamento_abm = DepartamentoABM()
 	carrera_abm = CarreraABM()
 
-	departamento_abm.insertar('departamento1')
-	carrera_abm.insertar('carrera1', 1)
+	#departamento_abm.eliminar(1)
+	#departamento_abm.eliminar(2)
 
+	#carrera_abm.eliminar(1)
+	#carrera_abm.eliminar(2)
+
+	departamento_abm.insertar('departamento1')
+	departamento_abm.insertar('departamento2')
+	departamento_abm.insertar('departamento3')
+
+
+	carrera_abm.insertar('Departamento de Ciencias Básicas', 1)
+	carrera_abm.insertar('Departamento de Ingeniería Civil', 2)
+	carrera_abm.insertar('Departamento de Ingeniería Eléctrica', 3)
+	carrera_abm.insertar('Departamento de Ingeniería Electrónica', 1)
+	carrera_abm.insertar('Departamento de Ingeniería Industrial', 2)
+	carrera_abm.insertar('Departamento de Ingeniería Mecánica', 3)
+	carrera_abm.insertar('Departamento de Ingeniería Naval', 1)
+	carrera_abm.insertar('Departamento de Ingeniería Quimica', 2)
+	carrera_abm.insertar('Departamento de Ingeniería en Sistemas de Información', 3)
+	carrera_abm.insertar('Departamento de Ingeniería Textil', 1)
 
 	#dejo comentado este traer que funciona, creo
 	#for a in abm.traerPorProfesor('didio'):
