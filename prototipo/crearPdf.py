1cd040a2f83b3e97291ee0a5bd688d05cbfab7f4
LuceroJose <33098113+LuceroJose@users.noreply.github.com>
2018-11-10 11:02:16 -0300

    Pdf crear aula

--- prototipo/crearPdf.py
+++ prototipo/crearPdf.py
@@ -1,21 +1,41 @@
 from reportlab.pdfgen import canvas
+import time
 
 
 def crear_aula(departamento, carrera, nombreaula, nombredocente, apellidodocente,
                dni, emailprofesor, rol, descripcion):
     c = canvas.Canvas("crear_aula.pdf")
-    c.drawString(100, 750, departamento)
+    c.setLineWidth(.3)
+    c.setFont('Helvetica', 12)
+    c.drawString(85, 630, 'Departameto:')
+    c.drawString(95, 615, departamento)
     if departamento == '11':
-        c.drawString(100, 740, carrera)
+        c.drawString(85, 585, 'Carrera:')
+        c.drawString(95, 570, carrera)
     else:
-        c.drawString(100, 740, '------------')
-    c.drawString(100, 730, nombreaula)
-    c.drawString(100, 720, nombredocente)
-    c.drawString(100, 710, apellidodocente)
-    c.drawString(100, 700, dni)
-    c.drawString(100, 690, emailprofesor)
-    c.drawString(100, 680, rol)
-    c.drawString(100, 670, descripcion)
+        c.drawString(85, 585, 'Nombre del aula:')
+        c.drawString(95, 570, nombreaula)
+    c.drawString(85, 540, 'Nombre del Docente:')
+    c.drawString(95, 525, nombredocente)
+    c.drawString(85, 495, 'Apellido del docente:')
+    c.drawString(95, 480, apellidodocente)
+    c.drawString(85, 450, 'Dni:')
+    c.drawString(95, 435, dni)
+    c.drawString(85, 405, 'Email del profesor:')
+    c.drawString(95, 390, emailprofesor)
+    c.drawString(85, 360, 'Rol del profesor:')
+    c.drawString(95, 345, rol)
+    c.drawString(85, 315, 'Descripcion:')
+    c.drawString(95, 300, descripcion)
+    c.drawString(430, 765, time.strftime("%c"))
+    c.drawImage("static\silversonic.jpg", 80, 780, 50, 50)
+    c.line(60, 760, 535, 760)
+    c.line(85, 665, 205, 665)
+    c.setFont('Helvetica', 15)
+    c.drawString(85, 670, 'Datos ingresados:')
+    c.setFont('Helvetica', 18)
+    c.drawString(170, 800, 'Universidad Tecnologica Nacional')
+    c.drawString(200, 720, 'Solicitud: Crear aula')
 
     c.save()
 
